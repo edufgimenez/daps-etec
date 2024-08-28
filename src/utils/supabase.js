051dacc69878bb92100e91970aeaa24c54b04f2b
Eduardo Gimenez <edufgimenez@hotmail.com>
2024-08-28 03:56:47 -0300

    teste commit gitignore2

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Load environment variables from .env file
 const SUPABASE_URL = '1';
-const SUPABASE_ANON_KEY = '1';
+const SUPABASE_ANON_KEY = '2';
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
